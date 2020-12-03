ef7cd3795f8e8a9bfdd5c8312e8367aa9149aff1
Karthik Ramachandran <karthik.gogeta@gmail.com>
2020-12-03 12:00:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+remaining_years = 80 - int(age)
+
+print(f"You have {remaining_years * 365} days, {remaining_years * 52} weeks and {remaining_years * 12} months left")
